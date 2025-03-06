d4cf218f1e819b533f195d4a6d500971a432addc
M2K Developments <m2ksoftiez@gmail.com>
2025-03-06 13:26:04 +0200

    v4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='bible-chichewa',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages('src'),
     package_dir = {'':'src'},
     classifiers=[
