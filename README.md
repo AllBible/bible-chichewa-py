bb54180375be0eecce1deaf78198d0d4e5496542
M2K Developments <m2ksoftiez@gmail.com>
2025-03-06 11:30:37 +0200

    Update on code snippets for ReadMe

--- README.md
+++ README.md
@@ -9,6 +9,82 @@
 
 Chichewa Bible pip package: Access and integrate the Chichewa translation of the Bible into your python applications with ease. Simplify verse retrieval, text search, and more using this lightweight and versatile pip package.
 
+
+## Features
+
+- Access verses, chapters, and books of the Chichewa Bible programmatically.
+- Retrieve text content in Chichewa for quoting, referencing, and display.
+- Flexible search functionality for finding specific passages or keywords.
+- Lightweight and easy-to-use API for seamless integration into Node.js applications.
+- Continuously updated to reflect the latest versions of the Chichewa Bible text.
+
+## Installation
+
+You can install the Chichewa Bible python package using pip:
+
+```
+pip install chichewa-bible
+```
+
+## Examples
+To use the bible-chichewa library to retrieve a verse from the Chichewa Bible, follow these steps:
+
+### Example: Retrieving a Bible Verse
+```
+bible = Bible()
+books = bible.getBooks()
+print(f"There are {len(books)} books in the bible")
+
+bookNumber = 1  # Genesis
+book = books[bookNumber-1]
+chapter = 1
+verse = 1
+
+verseText = bible.getVerse(bookNumber, chapter, verse)
+print(f"{book} {chapter}:{verse}: {verseText}")
+```
+
+
+### Example: Retrieving a Bible Chapter
+
+```
+bible = Bible()
+books = bible.getBooks()
+bookNumber = 1  # Genesis
+book = books[bookNumber-1]
+
+chapter = 1 
+
+chapterVerses = bible.getChapter(bookNumber, chapter)
+
+print(f"Chapter {book} {chapter}:")
+for i in range(0, len(chapterVerses)):
+    verse = i+1
+    print(f"{verse}. {chapterVerses[i]}\n")
+
+```
+
+### Example: Retrieving a Bible Verses within Range
+```
+bible = Bible()
+
+books = bible.getBooks()
+bookNumber = 2 ## Exodus
+book = books[bookNumber-1]
+
+chapter = 20
+verseStart = 1
+verseEnd = 5
+
+verses = bible.getVerses(bookNumber, chapter, verseStart, verseEnd)
+
+print(f"{book} {chapter}:{verseStart}-{verseEnd}:")
+for index in range(0, len(verses)):
+    verse = verses[index]
+    print(f"{verseStart + index}: {verse}\n")
+
+```
+
 <a href="https://www.buymeacoffee.com/m2kdevelopments" target="_blank">
-<img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" >
+<img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !importantwidth: 217px !important" >
 </a>
--- src/index.py
+++ src/index.py
@@ -8,7 +8,7 @@ import os
 Get Bible information
 """
 class Bible:
-        
+
     def __init__(self):
         # Get the current working directory
         self.current_directory = os.getcwd()
@@ -16,35 +16,35 @@ class Bible:
     def getBooks(self):
         """
         Get the names of all books in the Bible.
-        
+
         :return: A list of Bible book names.
         """
         relative_path = "src\\content\\books.json"
         absolute_path = os.path.join(self.current_directory, relative_path)
-        
+
         with open(absolute_path, "r") as json_file:
             books = json.load(json_file)
             return list(map(lambda book: book['name'], books))
-            
-    def getChapter(self, book, chapter):
+
+    def getChapter(self, book: int, chapter: int):
         """
         Get the verses of a specific chapter from a book in the Bible.
-        
+
         :param book: The index of the Bible book (1-based).
         :param chapter: The chapter number.
         :return: A dictionary containing the chapter's verses.
         """
         relative_path = f"src\\resources\\{book}\\{chapter}.json"
         absolute_path = os.path.join(self.current_directory, relative_path)
-        
+
         with open(absolute_path, "r") as json_file:
             verses = json.load(json_file)
             return verses
-        
-    def getVerse(self, book, chapter, verse):
+
+    def getVerse(self, book: int, chapter: int, verse: int):
         """
         Get the content of a specific verse from a chapter in a book of the Bible.
-        
+
         :param book: The index of the Bible book (1-based).
         :param chapter: The chapter number.
         :param verse: The verse number.
@@ -52,10 +52,10 @@ class Bible:
         """
         return self.getChapter(book, chapter)[verse]
 
-    def getVerses(self, book, chapter, verseStart, verseEnd):
+    def getVerses(self, book, chapter: int, verseStart: int, verseEnd: int):
         """
         Get a range of verses from a chapter in a book of the Bible.
-        
+
         :param book: The index of the Bible book (1-based).
         :param chapter: The chapter number.
         :param verseStart: The starting verse number.
@@ -64,24 +64,24 @@ class Bible:
         """
         return self.getChapter(book, chapter)[verseStart-1:verseEnd]
 
-    def getChapterCount(self, book):
+    def getChapterCount(self, book: int):
         """
         Get the number of chapters in a specific book of the Bible.
-        
+
         :param book: The index of the Bible book (1-based).
         :return: The number of chapters in the book.
         """
         relative_path = "src\\content\\books.json"
         absolute_path = os.path.join(self.current_directory, relative_path)
-        
+
         with open(absolute_path, "r") as json_file:
             books = json.load(json_file)
             return list(map(lambda book: book['chapters'], books))[book-1]
 
-    def getVerseCount(self, book, chapter):
+    def getVerseCount(self, book: int, chapter: int):
         """
         Get the number of verses in a specific chapter of a book in the Bible.
-        
+
         :param book: The index of the Bible book (1-based).
         :param chapter: The chapter number.
         :return: The number of verses in the chapter.
